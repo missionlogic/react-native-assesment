ee79534bb55fdaa6bf763b1c84429c731ff287a3
Chris Pawlukiewicz <hi@paynoattn.com>
2019-07-21 17:45:54 -0600

    working starting code

--- App.tsx
+++ App.tsx
@@ -1,1 +1,30 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
 
+import { Home } from './app/Home';
+
+declare global {
+  interface Window { __REDUX_DEVTOOLS_EXTENSION__: any; }
+}
+
+// const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// optional ADD reducers
+// export const APP_STORE = createStore(
+//   combineReducers({
+//   }),
+//   devtools
+// );
+
+
+export default function App() {
+  // return (
+  //   <Provider store={APP_STORE} >
+  //     <Home />
+  //   </Provider>
+  // );
+  return (
+    <Home />
+  );
+}
--- app/Home.tsx
+++ app/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export function Home() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
